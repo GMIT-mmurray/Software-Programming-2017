a847211897e36d8b5ae2ea6c4b220677a449cd0b
GMADTREE\mmurray <michael.murray@gmit.ie>
2017-11-13 10:26:34 +0000

    a

---
+++ GroupC/sketch_nov13a/sketch_nov13a.ino
@@ -0,0 +1,22 @@
+int pinArray[] = {4, 6, 2, 3, 7, 5};
+int timeOut[] = {500, 1000, 3000, 400, 1000, 600};
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  for (int i = 0; i < 6; i++) {
+    pinMode(pinArray[i], OUTPUT);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    int data = Serial.read();
+    for (int i = 0; i < 6; i++) {
+      digitalWrite(pinArray[i], HIGH);
+      delay(timeOut[i]);
+      digitalWrite(pinArray[i], LOW);
+    }
+  }
+}
