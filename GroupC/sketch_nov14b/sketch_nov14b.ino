2f6116b9eca27202cffb678d8ac19c133bba14c0
GMADTREE\mmurray <michael.murray@gmit.ie>
2017-11-14 11:38:58 +0000

    2

--- GroupC/sketch_nov14b/sketch_nov14b.ino
+++ GroupC/sketch_nov14b/sketch_nov14b.ino
@@ -1,43 +1,43 @@
 int pinArray[] =  {4, 6, 2, 3, 7, 5};
 int pinArray1[] = {5, 4, 3, 7, 6, 2};
 int timeOut[] = {500, 1000, 3000, 400, 1000, 600};
-
+int val;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   for (int i = 0; i < 6; i++) {
     pinMode(pinArray[i], OUTPUT);
   }
- }
+  for (int i = 8; i < 14; i++) {
+    pinMode(i, INPUT);
+  }
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.available() > 0) {
-    int data = Serial.read();
-    switch (data) {
-      case '0':
-        blink(0);
-        break;
-      case '1':
-        blink(1);
-        break;
-      case '2':
-        blink(2);
-        break;
-      case '3':
-        blink(3);
-        break;
-      case '4':
-        blink(4);
-        break;
-      case '5':
-        blink(5);
-        break;
-      default:
-        Serial.print(" wrong key pressed ");
-        Serial.println(data);
-        break;
-    }
+  val = digitalRead(8);
+  if (val == HIGH) {
+    blink(0);
+  }
+  val = digitalRead(9);
+  if (val == HIGH) {
+    blink(1);
+  }
+  val = digitalRead(10);
+  if (val == HIGH) {
+    blink(2);
+  }
+  val = digitalRead(11);
+  if (val == HIGH) {
+    blink(3);
+  }
+  val = digitalRead(12);
+  if (val == HIGH) {
+    blink(4);
+  }
+  val = digitalRead(13);
+  if (val == HIGH) {
+    blink(5);
   }
 }
 
