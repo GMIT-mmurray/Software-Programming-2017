df15fbc124be14563034f56f2012e9c3401316db
GMADTREE\mmurray <michael.murray@gmit.ie>
2017-11-20 11:58:10 +0000

    final ver

--- GroupB/sketch_nov20a/sketch_nov20a.ino
+++ GroupB/sketch_nov20a/sketch_nov20a.ino
@@ -6,6 +6,8 @@ int button1PrevValue = 0;
 int button2Value = 1;
 int button2PrevValue = 1;
 
+int counter = 0;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(button1, INPUT);
@@ -14,18 +16,30 @@ void setup() {
 }
 
 void loop() {
+  // read the pushbutton input pin:
   button1Value = digitalRead(button1); // Present Value
+  // compare the buttonState to its previous state
   if (button1Value != button1PrevValue) {
+    // if the current state is HIGH then the button
+    // went from low to high:
     if (button1Value == HIGH) {
-      Serial.println("Gone HIGH");
+      if (++counter > 16) {
+        counter = 16;
+      }
+      Serial.println(counter);
     }
   }
+  // save the current value into trevious value,
+  //for next time through the loop
   button1PrevValue = button1Value;
 
-  button1Value = digitalRead(button2); // Present Value
+  button2Value = digitalRead(button2); // Present Value
   if (button2Value != button2PrevValue) {
     if (button2Value == LOW) {
-      Serial.println("Gone LOW");
+      if (--counter < 0) {
+        counter = 0;
+      }
+      Serial.println(counter);
     }
   }
   button2PrevValue = button2Value;
