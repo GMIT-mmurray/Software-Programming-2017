4aa57c180c91eca10a332ee75aa399efaf0e4d44
GMADTREE\mmurray <michael.murray@gmit.ie>
2017-12-01 11:32:59 +0000

    a#2

--- GroupA/sketch_dec01a/sketch_dec01a.ino
+++ GroupA/sketch_dec01a/sketch_dec01a.ino
@@ -16,4 +16,87 @@ void loop() {
   int bit0 = digitalRead(pinInputs[0]);
   int bit1 = digitalRead(pinInputs[1]);
   int bit2 = digitalRead(pinInputs[2]);
+
+  if (bit0 == 0 && bit1 == 0 && bit2 == 0) {
+    digitalWrite(pinOutputs[0], HIGH);
+    digitalWrite(pinOutputs[1], LOW);
+    digitalWrite(pinOutputs[2], LOW);
+    digitalWrite(pinOutputs[3], LOW);
+    digitalWrite(pinOutputs[4], LOW);
+    digitalWrite(pinOutputs[5], LOW);
+    digitalWrite(pinOutputs[6], LOW);
+    digitalWrite(pinOutputs[7], LOW);
+  }
+
+  if (bit0 == 1 && bit1 == 0 && bit2 == 0) {
+    digitalWrite(pinOutputs[0], LOW);
+    digitalWrite(pinOutputs[1], HIGH);
+    digitalWrite(pinOutputs[2], LOW);
+    digitalWrite(pinOutputs[3], LOW);
+    digitalWrite(pinOutputs[4], LOW);
+    digitalWrite(pinOutputs[5], LOW);
+    digitalWrite(pinOutputs[6], LOW);
+    digitalWrite(pinOutputs[7], LOW);
+  }
+    if (bit0 == 0 && bit1 == 0 && bit2 == 0) {
+    digitalWrite(pinOutputs[0], HIGH);
+    digitalWrite(pinOutputs[1], LOW);
+    digitalWrite(pinOutputs[2], LOW);
+    digitalWrite(pinOutputs[3], LOW);
+    digitalWrite(pinOutputs[4], LOW);
+    digitalWrite(pinOutputs[5], LOW);
+    digitalWrite(pinOutputs[6], LOW);
+    digitalWrite(pinOutputs[7], LOW);
+  }
+    if (bit0 == 0 && bit1 == 0 && bit2 == 0) {
+    digitalWrite(pinOutputs[0], HIGH);
+    digitalWrite(pinOutputs[1], LOW);
+    digitalWrite(pinOutputs[2], LOW);
+    digitalWrite(pinOutputs[3], LOW);
+    digitalWrite(pinOutputs[4], LOW);
+    digitalWrite(pinOutputs[5], LOW);
+    digitalWrite(pinOutputs[6], LOW);
+    digitalWrite(pinOutputs[7], LOW);
+  }
+    if (bit0 == 0 && bit1 == 0 && bit2 == 0) {
+    digitalWrite(pinOutputs[0], HIGH);
+    digitalWrite(pinOutputs[1], LOW);
+    digitalWrite(pinOutputs[2], LOW);
+    digitalWrite(pinOutputs[3], LOW);
+    digitalWrite(pinOutputs[4], LOW);
+    digitalWrite(pinOutputs[5], LOW);
+    digitalWrite(pinOutputs[6], LOW);
+    digitalWrite(pinOutputs[7], LOW);
+  }
+    if (bit0 == 0 && bit1 == 0 && bit2 == 0) {
+    digitalWrite(pinOutputs[0], HIGH);
+    digitalWrite(pinOutputs[1], LOW);
+    digitalWrite(pinOutputs[2], LOW);
+    digitalWrite(pinOutputs[3], LOW);
+    digitalWrite(pinOutputs[4], LOW);
+    digitalWrite(pinOutputs[5], LOW);
+    digitalWrite(pinOutputs[6], LOW);
+    digitalWrite(pinOutputs[7], LOW);
+  }
+    if (bit0 == 0 && bit1 == 0 && bit2 == 0) {
+    digitalWrite(pinOutputs[0], HIGH);
+    digitalWrite(pinOutputs[1], LOW);
+    digitalWrite(pinOutputs[2], LOW);
+    digitalWrite(pinOutputs[3], LOW);
+    digitalWrite(pinOutputs[4], LOW);
+    digitalWrite(pinOutputs[5], LOW);
+    digitalWrite(pinOutputs[6], LOW);
+    digitalWrite(pinOutputs[7], LOW);
+  }
+    if (bit0 == 0 && bit1 == 0 && bit2 == 0) {
+    digitalWrite(pinOutputs[0], HIGH);
+    digitalWrite(pinOutputs[1], LOW);
+    digitalWrite(pinOutputs[2], LOW);
+    digitalWrite(pinOutputs[3], LOW);
+    digitalWrite(pinOutputs[4], LOW);
+    digitalWrite(pinOutputs[5], LOW);
+    digitalWrite(pinOutputs[6], LOW);
+    digitalWrite(pinOutputs[7], LOW);
+  }
+
 }
