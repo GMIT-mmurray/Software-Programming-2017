696ac01dbd4a665d918f75ca098dfef8d040f8c3
GMADTREE\mmurray <michael.murray@gmit.ie>
2017-12-04 08:17:59 +0000

    a

--- GroupB/sketch_nov24a/sketch_nov24a.ino
+++ GroupB/sketch_nov24a/sketch_nov24a.ino
@@ -2,6 +2,7 @@
 const int SPR = 200;
 Stepper stepper1(SPR, 8, 9);
 Stepper stepper2(SPR, 10, 11);
+int previousValue = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -19,10 +20,14 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0) {
-    int number = Serial.parseInt();
-    Serial.print(" Steps : ");
-    Serial.println(number/1.8);
-    stepper1.step(number/1.8);
-    stepper2.step((number*-1)/1.8);
+    int PresentValue = Serial.parseInt();
+    Serial.print(" Dgrees : ");
+    Serial.println(PresentValue);
+    stepper1.step((PreviousValue*-1)/1.8);
+    delay(50);
+    stepper1.step(PresentValue/1.8);
+    delay(50);
+    PreviousValue = PresentValue;
+  
   }
 }
